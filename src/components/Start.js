83b1b40a94cc10e9ce099557dc778f5c12cbf982
Arrangedgodly <arrangedgodly@gmail.com>
2023-01-12 19:20:09 -0700

    refactor

--- src/components/Start.js
+++ src/components/Start.js
@@ -1,24 +1,21 @@
-import { Box, useColorModeValue, Heading, Spacer, Text, Select } from "@chakra-ui/react";
+import { Box, Button, useColorModeValue, Heading, Spacer, Text, Fade, useDisclosure } from "@chakra-ui/react";
+import StoreSelect from "./StoreSelect";
 
 function Start() {
   const bg = useColorModeValue('gray.300', 'gray.600');
+  const { isOpen, onToggle } = useDisclosure();
+
   return (
     <Box bg={bg} w='750px' h='750px' marginTop='5vh' borderRadius='25px' display='flex' flexDirection='column' alignItems='center' justifyContent='center'>
       <Heading>Welcome!</Heading>
-      <Text>To get started, fill out the form below.</Text>
-      <Spacer />
-      <Select placeholder="Select your Store :">
-        <option value='Broadmoor'>Broadmoor</option>
-        <option value='Colfax'>Colfax</option>
-        <option value='Fraser'>Fraser</option>
-        <option value='Littleton'>Littleton</option>
-        <option value='Nederlands'>Nederlands</option>
-        <option value='Northfield'>Northfield</option>
-        <option value='Quincy'>Quincy</option>
-        <option value='Rockrimmon'>Rockrimmon</option>
-        <option value='Sheridan'>Sheridan</option>
-      </Select>
-      <Spacer />
+      <Text>To get started, click <Button onClick={onToggle}>here.</Button></Text>
+      <Fade in={isOpen}>
+        <Spacer />
+        <form>
+          <StoreSelect />
+        </form>
+        <Spacer />
+      </Fade>
     </Box>
   );
 }
